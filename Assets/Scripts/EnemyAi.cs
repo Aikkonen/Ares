3ba71a446ca4225b935cfa8de72af42b2b065a5e
Lauri Ikonen <Lauri Ikonen>
2016-03-19 22:35:16 +0200

    new and improved enemy AI

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -6,17 +6,25 @@ public class EnemyAi : MonoBehaviour
 
     public GameObject player;
     private NavMeshAgent agent;
+    public bool playerInSight = false;
+    public Vector3 playerLastSighted;
+    public float enemyFieldOfView = 120;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
-        agent.SetDestination(player.transform.position);
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        Vector3 vectorFromEnemyToPlayer = player.transform.position - transform.position;
+        Vector3 forward = transform.forward;
+
+        float angleToPlayer = Vector3.Angle(vectorFromEnemyToPlayer, forward);
+
+        if (dist < 10 && angleToPlayer < enemyFieldOfView * 0.5)
+        {
+            agent = gameObject.GetComponent<NavMeshAgent>();
+            agent.SetDestination(player.transform.position);
+        }
     }
 }
+
