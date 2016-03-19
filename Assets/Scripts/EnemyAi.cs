8c1654504e8b3ae582f0deb776fb63535108d2e5
Lauri Ikonen <Lauri Ikonen>
2016-03-19 20:21:15 +0200

    Enemy model and AI

---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAi : MonoBehaviour
+{
+
+    public GameObject player;
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.SetDestination(player.transform.position);
+    }
+}
